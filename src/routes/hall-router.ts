516a2c8967a4da4f7120ec943c6118cb8ff82e5e
ralphZero <placideralph@gmail.com>
2022-08-27 11:56:18 -0400

    fixing returned json

--- src/routes/hall-router.ts
+++ src/routes/hall-router.ts
@@ -7,11 +7,15 @@ export const hallRouter = Router();
 hallRouter.get('/:uid', async (req: Request, res: Response) => {
     const { uid } = req.params;
     const halls = await HallServices.getHallsOfCurrentUser(uid);
-    res.status(200).json(halls);
+    res.status(200).json({result: halls, success: true});
 });
 
 hallRouter.post('/', async (req: Request, res: Response) => {
     const newHall: Hall = req.body
-    const result = await HallServices.createHallAndReturnIt(newHall)
-    res.status(201).json(result);
+    if (!newHall.userId) {
+        res.status(400).json({ success: false, message: "A user is required." })
+    } else {
+        const result = await HallServices.createHallAndReturnIt(newHall)
+        res.status(201).json({result, success: true});
+    }
 });
