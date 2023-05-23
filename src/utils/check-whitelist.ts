ee5bb160aa67cfc04c7152c1981e9b91d1cad840
ralphZero <placideralph@gmail.com>
2023-05-22 23:39:43 -0400

    added white list to dev

---
+++ src/utils/check-whitelist.ts
@@ -0,0 +1,38 @@
+import { NextFunction } from 'express';
+import { isDevelop } from './environment';
+import { DecodedIdToken } from 'firebase-admin/auth';
+
+interface CallBackError {
+  message: string;
+  code: number;
+}
+
+export const checkWhitelist = (
+  result: DecodedIdToken,
+  next: NextFunction,
+  errorCallback: (error: CallBackError) => void
+) => {
+  if (isDevelop()) {
+    const whiteList = JSON.parse(process.env.IAM_WHITELIST as string);
+
+    if (Array.isArray(whiteList)) {
+      const index = (
+        whiteList as Array<{ email: string; uid: string }>
+      ).findIndex(
+        (entry) => entry.email === result.email && entry.uid === result.uid
+      );
+
+      if (index !== -1) {
+        next();
+      } else {
+        errorCallback({ code: 401, message: 'Unauthorized access' });
+        return;
+      }
+    } else {
+      errorCallback({ code: 500, message: 'Internal server error' });
+      return;
+    }
+  } else {
+    next();
+  }
+};
