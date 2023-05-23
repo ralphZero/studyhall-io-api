124b173359bc4723731e78ff68c1acc5cdb36f85
ralphZero <placideralph@gmail.com>
2023-05-22 20:33:40 -0400

    test env

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import * as functions from 'firebase-functions';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { hallRouter } from './src/routes/hall-router';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/test', (req: Request, res: Response) => {
+  res.json({ env: process.env.NODE_ENV });
+});
+
 app.use('/', hallRouter);
 
 export const devApi = app;
