1e2b89664feb956e9f9fd5c22816775525504d69
ralphZero <placideralph@gmail.com>
2023-05-23 21:18:45 -0400

    remove test route

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import { hallRouter } from './src/routes/hall-router';
 import * as dotenv from 'dotenv';
+import { isDevelop } from './src/utils/environment';
+import { planRouter } from './src/routes/plan-router';
 
 dotenv.config();
 const app = express();
@@ -10,11 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', (req: Request, res: Response) => {
-  res.json({ env: process.env.NODE_ENV });
-});
-
 app.use('/', hallRouter);
+
+if (isDevelop()) {
+  app.use('/v2/', planRouter);
+}
 
 export const devApi = app;
 export const api = functions.https.onRequest(app);
